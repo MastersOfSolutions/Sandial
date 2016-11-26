88c096391a416071513bc8d8c9ce8c7ac2beb91c
Ethan Randall <ijunkie22@gmail.com>
2016-11-26 12:55:23 -0500

    Added styling and changed to iframe

--- js/sandial-clock.js
+++ js/sandial-clock.js
@@ -1,35 +1,72 @@
 // Sandial clock logic
-var startTimeout;
+var calibrateTimer = null;
+var tickTimer = null;
 var clockEl;
+var inBG = 0;
+
+function stopClock() {
+  if (tickTimer !== null) {
+    window.clearInterval(tickTimer);
+    return true;
+  }
+  return false;
+}
+
+function validateClock() {
+  var dateNow = new Date();
+  var clockImgSrcNow = clockSrcFormat(dateNow.getHours(), dateNow.getMinutes());
+  return clockImgSrcNow == clockEl.getAttribute("src");
+}
 
 function startClock() {
-  window.clearTimeout(startTimeout);
+  stopClock();
+  if (calibrateTimer !== null) {
+    window.clearTimeout(calibrateTimer);
+  }
   clockTick();
-  window.setTimeout(clockTick, 60000);
+  tickTimer = window.setInterval(clockTick, 60000);
 }
 
 function calibrate() {
+  console.log("Calibrating the clock.");
   clockEl = document.getElementById("sandial-clock-img");
   var msOffset = (60 - new Date().getSeconds()) * 1000;
-  startTimeout = window.setTimeout(startClock, msOffset);
+  calibrateTimer = window.setTimeout(startClock, msOffset);
   clockTick();
+}
+
+function clockSrcFormat(hourInt, minsInt) {
+  var hourStr = (hourInt < 10) ? "0" + hourInt.toString() : hourInt.toString();
+  var minsStr = (minsInt < 10) ? "0" + minsInt.toString() : minsInt.toString();
+  return "clocks/clock_" + hourStr + "_" + minsStr + ".svg";
 }
 
 function clockTick() {
   var dateNow = new Date();
-  var hourNow = dateNow.getHours();
-  var minsNow = dateNow.getMinutes();
-  var hourStr = hourNow.toString();
-  if (hourNow < 10) {
-    hourStr = "0" + hourStr;
-  }
-  var minsStr = minsNow.toString();
-  if (minsNow < 10) {
-    minsStr = "0" + minsStr;
-  }
-  
-  var clockImgSrc = "clocks/clock_" + hourStr + "_" + minsStr + ".svg";
+  var clockImgSrc = clockSrcFormat(dateNow.getHours(), dateNow.getMinutes());
+  console.log("Changing clock to " + clockImgSrc);
   clockEl.src = clockImgSrc;
 }
 
+function handleOnblur(blurEvt) {
+  inBG++;
+}
+
+function handleOnFocus(focusEvt) {
+  if (inBG > 0) {
+    console.log("Focus regained. Validating that we didn't fall out of sync while in background.");
+    inBG = 0;
+    if (!validateClock()) {
+      calibrate();
+    }
+    else {
+      console.log("Everything looks okay.");
+    }
+  }
+  
+}
+
 calibrate();
+
+document.body.onfocus = handleOnFocus;
+document.body.onblur = handleOnblur;
